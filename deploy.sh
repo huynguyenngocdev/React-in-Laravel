8b45d391f541a8bc466b87cda977d6d88fc3be04
huynguyenngocdev <ngochuy2k1vinhdinh@gmail.com>
2021-06-24 13:43:30 +0700

    Upload to Heroku.

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#Run DB migrate
+php artisan migrate:fresh
+
+# Run Seeds
+php artisan db:seed
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        return $request->user();
 });
 
 Route::get('/contact', 'ContactController@index')->name('contact.all');
