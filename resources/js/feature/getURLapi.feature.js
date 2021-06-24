8a18cc71c4c894daa95ba0521bb3e6d52d2791a7
huynguyenngocdev <ngochuy2k1vinhdinh@gmail.com>
2021-06-24 14:45:49 +0700

    Fix url of api.

--- resources/js/feature/getURLapi.feature.js
+++ resources/js/feature/getURLapi.feature.js
@@ -1,8 +1,9 @@
 const getURLapi = () => {
-    var parser = document.createElement("a");
-    parser.href = window.location.href;
-    const url = `${parser.protocol}//${parser.host}`;
-    return url;
+    // var parser = document.createElement("a");
+    // parser.href = window.location.href;
+    // const url = `${parser.protocol}//${parser.host}`;
+    // return url;
+    return "https://react-laravel-app.herokuapp.com";
 };
 
 export default getURLapi;
